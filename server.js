d7878ca52ae519c42cb001515f0a0995ef2da151
mattsald48 <matt.saldana8@gmail.com>
2017-10-09 12:48:14 -0500

    Changed file structure

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.set("view engine", "handlebars");
 
 
 //Import routes and give the server access to them
-var routes = require("./app/controllers/burgers_controller.js");
+var routes = require("./controllers/burgers_controller.js");
 app.use("/", routes);
 
 //Starting Express App
